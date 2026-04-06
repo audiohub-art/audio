650ef29be13ad4a263974312cadc6cbadae5c25e
antoineFabr <pt52png@eduvaud.ch>
2026-04-06 11:47:36 +0200

    [fix] Remove debug log

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
     });
 
     if (existingUser) {
-      throw new ConflictException('This name is already take');
+      throw new ConflictException('This name is already taken');
     }
     const hashedPassword = await bcrypt.hash(registerDto.password, 12);
     const user = await this.prisma.users.create({
@@ -43,32 +43,19 @@ export class AuthService {
       where: { name: loginDto.name },
     });
     if (!user || !(await bcrypt.compare(loginDto.password, user.password))) {
-      throw new UnauthorizedException('Identifiants invalides');
+      throw new UnauthorizedException('Invalid credentials');
     }
     return await this.generateAndStoreTokens(user.id, user.name);
   }
 
   async refresh(userId: number, refreshToken: string) {
-    console.log('user id : ', userId);
-    console.log('refreshToken', refreshToken);
     const user = await this.prisma.users.findUnique({
       where: { id: userId },
     });
-    console.log('hashed refresh token in base : ', user?.refreshToken);
     if (!user || !user.refreshToken) {
       throw new ForbiddenException('Access denied');
     }
-    console.log(
-      'token reçu (50 premiers chars):',
-      refreshToken.substring(0, 50),
-    );
-    console.log(
-      'hash en base (50 premiers chars):',
-      user.refreshToken.substring(0, 50),
-    );
-    console.log('longueur token reçu:', refreshToken.length);
     const isValid = this.hashToken(refreshToken) === user.refreshToken;
-    console.log('is valide', isValid);
     if (!isValid) {
       await this.prisma.users.update({
         where: { id: userId },
@@ -85,7 +72,6 @@ export class AuthService {
 
   private async generateAndStoreTokens(userId: number, name: string) {
     const payload = { sub: userId, name };
-    console.log('payload', payload);
     const [accessToken, refreshToken] = await Promise.all([
       this.jwtService.signAsync(payload, {
         secret: this.configService.get('JWT_ACCESS_SECRET'),
@@ -96,12 +82,10 @@ export class AuthService {
         expiresIn: '7d',
       }),
     ]);
-    console.log('new refresh token : ', refreshToken);
-    const user = await this.prisma.users.update({
+    await this.prisma.users.update({
       where: { id: userId },
       data: { refreshToken: this.hashToken(refreshToken) },
     });
-    console.log('hashed refresh token', user.refreshToken);
     return { accessToken, refreshToken };
   }
 }
